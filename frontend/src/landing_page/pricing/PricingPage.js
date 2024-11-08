fc0ac3388355420db97fc8331d7e5bc23baaa355
Saniya Pawankar <saniyapawankar@gmail.com>
2024-11-08 23:23:24 +0530

    First Commit

---
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Brokerage from './Brokerage';
+import Hero from './Hero';
+import Navbar from '../../Navbar';
+import Footer from '../../Footer';
+function PricingPage() {
+    return ( 
+        <>
+        <Navbar/>
+        <Brokerage/>
+        <Hero/>
+        <Footer/>
+        </>
+     );
+}
+
+export default PricingPage;
---
+++ frontend/src/landing_page/support/SupportPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CreateTicket from './CreateTicket';
+import Hero from './Hero';
+import Navbar from '../../Navbar';
+import Footer from '../../Footer';
+function SupportPage() {
+    return (  
+    <>
+    <Navbar/>
+    <CreateTicket/>
+    <Hero/>
+    <Footer/>
+    </>
+    );
+}
+
+export default SupportPage;
